610cdb1480e09c5b858489c0a121e4a5663693b1
KevinJavaLee <1401597760@qq.com>
2020-07-02 09:20:04 +0800

    day30练习的代码

---
+++ src/com/kevin/day30/TestGit.java
@@ -0,0 +1,12 @@
+package com.kevin.day30;
+
+/**
+ * @author kevin
+ * @project Java30天核心技术
+ * @create 2020-07-02-9:19
+ */
+public class TestGit {
+    public static void main(String[] args) {
+        System.out.println("第一次使用git");
+    }
+}
