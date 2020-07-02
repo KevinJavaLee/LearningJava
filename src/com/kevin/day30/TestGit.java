9b73567928835e5606235a48ff13794c84f98bea
KevinXiao <31833513+KevinJavaLee@users.noreply.github.com>
2020-07-02 10:04:07 +0800

    在线修改

--- src/com/kevin/day30/TestGit.java
+++ src/com/kevin/day30/TestGit.java
@@ -8,5 +8,6 @@ package com.kevin.day30;
 public class TestGit {
     public static void main(String[] args) {
         System.out.println("第一次使用git");
+        System.out.println("Git的使用");
     }
 }
